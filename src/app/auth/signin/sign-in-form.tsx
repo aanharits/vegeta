e050d2e3b89793636b1e34a463d5b56b421893d1
Farhan Harits Prakoso <farhan.harits05@gmail.com>
2025-10-06 17:41:34 +0700

    feat(auth): integrate React Hook Form for auth forms

--- src/app/auth/signin/sign-in-form.tsx
+++ src/app/auth/signin/sign-in-form.tsx
@@ -6,37 +6,53 @@ import { hover } from "@/lib/hover";
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SignInSchema } from "@/validations/auth-validations";
+import { LoginForm } from "@/types/auth";
 
 function SignInForm() {
   const [showPassword, setShowPassword] = useState(false);
 
-  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    resolver: yupResolver(SignInSchema),
+  });
+
+  const onSubmit = (data: LoginForm) => {
+    console.log("data: ", data);
+  };
 
   return (
-    <form className="flex flex-col w-[100%] gap-4 items-center">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-[100%] gap-4">
       <div className="w-[100%] text-3xl font-semibold tracking-widest mb-2 text-center">
         Masuk akun anda
       </div>
-      <Input
-        className="w-[100%] p-4 rounded-sm"
-        type="text"
-        placeholder="Email"
-      />
-      <div className="w-[100%] relative">
+      <div className="w-[100%] relative flex flex-col gap-1 mt-4">
         <Input
-          className="w-[100%] p-4 rounded-sm"
+          className="w-[100%] p-4 rounded"
+          type="text"
+          placeholder="Email"
+          {...register("email")}
+          error={errors.email?.message}
+        />
+        <Input
+          className="w-[100%] p-4 rounded-sm mt-4"
           type={showPassword ? "text" : "password"}
           placeholder="Kata Sandi"
           suffix="Eye"
           onPressSuffix={() => setShowPassword(!showPassword)}
+          {...register("password")}
+          error={errors.password?.message}
         />
       </div>
 
       <Button
-        className={cn("w-[320px] bg-leaf mt-6", hover.shadow)}
-        onClick={() => {
-          router.push("/");
-        }}
+        className={cn("w-[320px] bg-leaf mt-6 mx-auto", hover.shadow)}
+        type="submit"
       >
         Masuk
       </Button>
--- src/app/auth/signup/sign-up-form.tsx
+++ src/app/auth/signup/sign-up-form.tsx
@@ -5,50 +5,74 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { hover } from "@/lib/hover";
 import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SignUpSchema } from "@/validations/auth-validations";
+import { RegisterForm } from "@/types/auth";
 
 function SignUpForm() {
   const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmationPassword, setShowConfirmationPassword] =
-    useState(false);
+  const [showConfirmationPassword, setShowConfirmationPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterForm>({
+    resolver: yupResolver(SignUpSchema),
+  });
+
+  const onSubmit = (data: RegisterForm) => {
+    console.log("data: ", data);
+  };
 
   return (
-    <form className="flex flex-col w-[100%] gap-4 items-center">
-      <div className="w-[100%] text-3xl font-semibold tracking-widest mb-2 text-center">
+    <form
+      onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-[100%] gap-4">
+      <div className="w-[100%] text-3xl font-semibold tracking-widest mb-2 mt-14 text-center">
         Buat akun baru
       </div>
-      <Input
-        className="w-[100%] p-4 rounded-sm"
-        type="text"
-        placeholder="Nama Lengkap"
-      />
-      <Input
-        className="w-[100%] p-4 rounded-sm"
-        type="text"
-        placeholder="Email"
-      />
-
-      <div className="w-[100%] relative">
+      <div className="w-[100%] relative flex flex-col gap-1">
         <Input
           className="w-[100%] p-4 rounded-sm"
+          type="text"
+          placeholder="Nama Lengkap"
+          {...register("name")}
+          error={errors.name?.message}
+        />
+        <Input
+          className="w-[100%] p-4 rounded-sm mt-2"
+          type="text"
+          placeholder="Email"
+          {...register("email")}
+          error={errors.email?.message}
+        />
+        <Input
+          className="w-[100%] p-4 rounded-sm mt-2"
           type={showPassword ? "text" : "password"}
           placeholder="Kata Sandi"
           suffix="Eye"
           onPressSuffix={() => setShowPassword(!showPassword)}
+          {...register("password")}
+          error={errors.password?.message}
         />
-      </div>
-      <div className="w-[100%] relative">
         <Input
-          className="w-[100%] p-4 rounded-sm"
+          className="w-[100%] p-4 rounded-sm mt-2"
           type={showConfirmationPassword ? "text" : "password"}
           placeholder="Konfirmasi Kata Sandi"
           suffix="Eye"
           onPressSuffix={() =>
             setShowConfirmationPassword(!showConfirmationPassword)
           }
+          {...register("confirmPassword")}
+          error={errors.confirmPassword?.message}
         />
       </div>
 
-      <Button className={cn("w-[320px] bg-leaf mt-6", hover.shadow)}>
+      <Button
+        type="submit"
+        className={cn("w-[320px] bg-leaf mt-6 mx-auto", hover.shadow)}
+      >
         Buat Akun
       </Button>
     </form>
